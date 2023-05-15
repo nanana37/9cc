18dc8278145a8816038c812fbe46b570169133f9
nanana37 <nakajima@keio.jp>
2023-05-15 08:51:13 +0000

    Add unary

--- 9cc.c
+++ 9cc.c
@@ -146,10 +146,6 @@ struct Node {
 	int val;
 };
 
-Node *expr();
-Node *mul();
-Node *primary();
-
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
 	Node *node = calloc(1, sizeof(Node));
 	node->kind = kind;
@@ -165,6 +161,12 @@ Node *new_node_num(int val) {
 	return node;
 }
 
+Node *expr();
+Node *mul();
+Node *unary();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
 Node *expr() {
 	Node *node = mul();
 
@@ -178,19 +180,30 @@ Node *expr() {
 	}
 }
 
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
 }
 
+// unary = ("+" | "-")? primary
+Node *unary() {
+	if (consume('+'))
+		return unary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), unary());
+	return primary();
+}
+
+// primary = num | "(" expr ")"
 Node *primary() {
 	if (consume('(')) {
 		Node *node = expr();
